aea3ccf5f3c6cea42e5e0c89ece03bc1a9eec7bb
Sebastián Rubio Quiroz <sebastian.rubio@iest.edu.mx>
2024-10-12 15:08:34 -0600

    First Commit

---
+++ CoreDataDemo/Persistence.swift
@@ -0,0 +1,24 @@
+//
+//  Persistence.swift
+//  CoreDataDemo
+//
+//  Created by Sebastián Rubio on 12/10/24.
+//
+
+import CoreData
+ 
+struct PersistenceController {
+    static let shared = PersistenceController()
+    
+    let container: NSPersistentContainer
+ 
+    init() {
+        container = NSPersistentContainer(name: "Products")
+        
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Container load failed: \(error)")
+            }
+        }
+    }
+}
