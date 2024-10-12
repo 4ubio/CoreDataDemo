f74d2461252bbf04d1027dde086229c605b4d3ac
Sebastián Rubio Quiroz <sebastian.rubio@iest.edu.mx>
2024-10-12 14:12:19 -0600

    Initial Commit

---
+++ CoreDataDemo/CoreDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataDemoApp.swift
+//  CoreDataDemo
+//
+//  Created by Sebastián Rubio on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoreDataDemoTests/CoreDataDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  CoreDataDemoTests.swift
+//  CoreDataDemoTests
+//
+//  Created by Sebastián Rubio on 12/10/24.
+//
+
+import Testing
+
+struct CoreDataDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
